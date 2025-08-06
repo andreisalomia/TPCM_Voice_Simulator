a81942ba3794f9493e93440ba6f2eda16afed97e
andrei.salomia <andreisalomia100@gmail.com>
2025-08-06 13:13:39 +0300

    Spring Boot Initialization

---
+++ src/main/java/com/example/tpcm_voice_simulator/TpcmVoiceSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.tpcm_voice_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpcmVoiceSimulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpcmVoiceSimulatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tpcm_voice_simulator/TpcmVoiceSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tpcm_voice_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpcmVoiceSimulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
